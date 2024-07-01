c453281c7a561a7317ff51d7caa411d15d8d1521
KS <kyusang.dev@gmail.com>
2024-07-01 11:11:34 +0900

    Simple Closure

--- Closure.playground/Contents.swift
+++ Closure.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 전통적인 함수 정의
+func descriptiveName() {
+    print("Run!")
+}
+
+// 전통적인 함수 실행
+descriptiveName()
+
+
+func multipleBy2 (x: Int) -> Int {
+    return x * 2
+}
+
+print(multipleBy2(x: 5))
+print(multipleBy2(x: 18))
+
+// Closure 상수
+let y = { (x: Int) -> Int in return x * 2 }
+
+print(y(5))
+print(y(18))
