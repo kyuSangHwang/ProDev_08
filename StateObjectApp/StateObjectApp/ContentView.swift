88593ea3fbc1a70bb88ce90abedfe6736e502d6f
KS <kyusang.dev@gmail.com>
2024-07-02 16:27:54 +0900

    StateObjectApp: StateObject 데이터 연동

--- StateObjectApp/StateObjectApp/ContentView.swift
+++ StateObjectApp/StateObjectApp/ContentView.swift
@@ -7,13 +7,24 @@
 
 import SwiftUI
 
+class AnimalModel: ObservableObject {
+    @Published var name: String = ""
+    @Published var breed: String = ""
+    @Published var age: Double = 0.0
+    @Published var weight: Double = 0.0
+}
+
 struct ContentView: View {
+    @StateObject var cat = AnimalModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, \(cat.name)")
+            Text("Hello, \(cat.breed)")
+            Text("Hello, \(cat.age)")
+            Text("Hello, \(cat.weight)")
+            
+            DisplayTextField(creature: cat)
         }
         .padding()
     }
---
+++ StateObjectApp/StateObjectApp/DisplayTextField.swift
@@ -0,0 +1,31 @@
+//
+//  DisplayTextField.swift
+//  StateObjectApp
+//
+//  Created by 황규상 on 7/2/24.
+//
+
+import SwiftUI
+
+struct DisplayTextField: View {
+    @ObservedObject var creature: AnimalModel
+    
+    var body: some View {
+        HStack {
+            Text("Name:")
+            TextField("Type a name here", text: $creature.name)
+        }
+        HStack {
+            Text("Breed:")
+            TextField("Type a breed here", text: $creature.breed)
+        }
+        HStack {
+            Text("Age:")
+            Slider(value: $creature.age, in: 0...20)
+        }
+        HStack {
+            Text("Weight:")
+            Slider(value: $creature.weight, in: 0...200)
+        }
+    }
+}
