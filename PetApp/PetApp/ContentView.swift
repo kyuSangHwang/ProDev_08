38942a306b62ee3f57d807d8e985621c73a11a0d
KS <kyusang.dev@gmail.com>
2024-07-02 13:48:15 +0900

    PetApp: ContentView 입력 UI 추가

--- PetApp/PetApp/ContentView.swift
+++ PetApp/PetApp/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let coreDM: CoreDataManager
+    @State var petName: String = ""
+    @State var petBreed: String = ""
+    @State var petArray: Array = [Animal]()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter pet name", text: $petName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Enter pet breed", text: $petBreed)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Save") {
+                coreDM.savePet(name: petName, breed: petBreed)
+                
+                petName = ""
+                petBreed = ""
+            }
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(coreDM: CoreDataManager())
 }
