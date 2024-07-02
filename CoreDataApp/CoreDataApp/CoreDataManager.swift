642e7228c6ef25314e6ebbd94865385395be60d5
KS <kyusang.dev@gmail.com>
2024-07-02 11:38:21 +0900

    CoreDataApp: CoreDataManager 추가

---
+++ CoreDataApp/CoreDataApp/CoreDataManager.swift
@@ -0,0 +1,22 @@
+//
+//  CoreDataManager.swift
+//  CoreDataApp
+//
+//  Created by 황규상 on 7/2/24.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    let persistentContainer: NSPersistentContainer
+    
+    init() {
+        persistentContainer = NSPersistentContainer(name: "CoreDataApp")
+        persistentContainer.loadPersistentStores { (description, error) in
+            if let error = error {
+                fatalError("Core Data failed to initialize \(error.localizedDescription)")
+            }
+        }
+    }
+}
