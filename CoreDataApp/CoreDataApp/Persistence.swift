06653a7358003e14928aa4e9ca9c2c0c45bc260b
KS <kyusang.dev@gmail.com>
2024-07-02 13:19:54 +0900

    CoreDataApp: 기본 템플릿 Animal 모델로 수정

--- CoreDataApp/CoreDataApp/Persistence.swift
+++ CoreDataApp/CoreDataApp/Persistence.swift
@@ -13,9 +13,10 @@ struct PersistenceController {
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+        for index in 0..<10 {
+            let newItem = Animal(context: viewContext)
+            newItem.name = "animal \(index)"
+            newItem.breed = "-"
         }
         do {
             try viewContext.save()
