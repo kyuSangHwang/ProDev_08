30e27d077e01eac9ffe7a2afe40b49f4dd8548ba
KS <kyusang.dev@gmail.com>
2024-07-02 11:22:31 +0900

    ReadWriteApp: 플레이스홀더와 같은 텍스트 입력에 대처

--- ReadWriteApp/ReadWriteApp/ContentView.swift
+++ ReadWriteApp/ReadWriteApp/ContentView.swift
@@ -9,25 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     let placeHolder = "텍스트를 입력하세요."
+    @State var userCreated = false
     @State var createText = "텍스트를 입력하세요."
     @State var displayText = ""
     
+    // 입력에 대한 포커스 상태 변수
     @FocusState var textFieldFocus: Bool
+    
+    var textDisabled: Bool {
+        placeHolder == createText && !textFieldFocus && !userCreated
+    }
+    
     var body: some View {
         VStack {
-            Text("Write")
-                .font(.headline)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 5)
             TextEditor(text: $createText)
-                .border(Color.gray, width: 0.5)
-                .foregroundStyle(placeHolder == createText ? .gray : .black)
+            // 플레이스홀더가 표시되어있으면, 텍스트 색상을 회색으로 변경
+                .foregroundStyle(textDisabled ? .gray : .black)
                 .focused($textFieldFocus)
                 .onChange(of: textFieldFocus) {
-                    if textFieldFocus, placeHolder == createText {
+                    // 텍스트필드에 포커스 이동시 플레이스홀더가 표시되어 있으면,
+                    // 입력 텍스트 필드를 초기화하고 입력 대기상태로 만듬
+                    if textFieldFocus, placeHolder == createText, !userCreated {
                         createText = ""
+                        userCreated = true
+                        // 다른 입력으로 포커스 이동시, 입력한 텍스트가 없으면,
+                        // 플레이스홀더 기본 텍스트로 대치
                     } else if !textFieldFocus, createText.isEmpty {
                         createText = placeHolder
+                        userCreated = false
                     }
                 }
             
