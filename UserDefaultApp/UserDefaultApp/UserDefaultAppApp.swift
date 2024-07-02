95703a284109bc6608746ce49fa5a19415df44db
KS <kyusang.dev@gmail.com>
2024-07-02 09:29:42 +0900

    Initialize Project UserDefaultApp

---
+++ UserDefaultApp/UserDefaultApp/UserDefaultAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultAppApp.swift
+//  UserDefaultApp
+//
+//  Created by 황규상 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
