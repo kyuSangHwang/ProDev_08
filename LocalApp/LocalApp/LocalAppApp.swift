f92c39fd5be8c99f985b1c0123e612ffb8a16740
KS <kyusang.dev@gmail.com>
2024-07-04 15:15:50 +0900

    Initialize LocalApp Project

---
+++ LocalApp/LocalApp/LocalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAppApp.swift
+//  LocalApp
+//
+//  Created by 황규상 on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
