034184d1d8ac78da66d873d4846ffd3018da1c50
KS <kyusang.dev@gmail.com>
2024-07-04 16:37:11 +0900

    CoreLocation 위치정보 업데이트

--- SearchLocationApp/SearchLocationApp/ContentView.swift
+++ SearchLocationApp/SearchLocationApp/ContentView.swift
@@ -14,30 +14,46 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            Map(position: $viewModel.carmeraPosition)
-                .mapStyle(viewModel.mapStyle)
-                .navigationTitle("SearchLocation")
-                .searchable(text: $viewModel.searchText)
-                .searchScopes($style) {
-                    Text("Standard").tag(0)
-                    Text("Imagery").tag(1)
-                    Text("Hybrid").tag(2)
-                }
-                .onChange(of: style) {
-                    switch style {
-                    case 0:
-                        viewModel.mapStyle = .standard
-                    case 1:
-                        viewModel.mapStyle = .imagery
-                    case 2:
-                        viewModel.mapStyle = .hybrid
-                    default:
-                        viewModel.mapStyle = .standard
+            ZStack(alignment: .bottom) {
+                Map(position: $viewModel.cameraPosition)
+                    .mapStyle(viewModel.mapStyle)
+                    .navigationTitle("SearchLocation")
+                    .searchable(text: $viewModel.searchText)
+                    .searchScopes($style) {
+                        Text("Standard").tag(0)
+                        Text("Imagery").tag(1)
+                        Text("Hybrid").tag(2)
                     }
+                    .onChange(of: style) {
+                        switch style {
+                        case 0:
+                            viewModel.mapStyle = .standard
+                        case 1:
+                            viewModel.mapStyle = .imagery
+                        case 2:
+                            viewModel.mapStyle = .hybrid
+                        default:
+                            viewModel.mapStyle = .standard
+                        }
+                    }
+                
+                HStack {
+                    Button(action: {
+                        viewModel.moveToCurrentLocation()
+                    }) {
+                        Image(systemName: "location.fill")
+                            .padding()
+                            .background(.white)
+                            .clipShape(Circle())
+                    }
+                    Spacer()
                 }
+                .padding()
+            }
         }
     }
 }
+
 
 #Preview {
     ContentView()
--- SearchLocationApp/SearchLocationApp/MapViewModel.swift
+++ SearchLocationApp/SearchLocationApp/MapViewModel.swift
@@ -9,8 +9,29 @@ import SwiftUI
 import MapKit
 
 @Observable
-class MapViewModel {
-    var carmeraPosition: MapCameraPosition = .userLocation(fallback: .automatic)    // 카메라 위치 설정 변수
+class MapViewModel: NSObject, CLLocationManagerDelegate {
+    var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)    // 카메라 위치 설정 변수
     var searchText: String = "" // 검색어 저장 변수
     var mapStyle: MapStyle = .standard  // 지도 스타일 설정 변수
+    
+    private var locationManager: CLLocationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func moveToCurrentLocation() {
+        cameraPosition = .userLocation(fallback: .automatic)
+    }
+    
+    // MARK: CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("error \(error.localizedDescription)")
+    }
 }
