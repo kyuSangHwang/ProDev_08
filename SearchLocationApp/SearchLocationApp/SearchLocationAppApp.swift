caa6f3fa828c8fcddd268adc947a38a92ab8526e
KS <kyusang.dev@gmail.com>
2024-07-04 16:03:09 +0900

    Initialize SearchLocationApp Project

---
+++ SearchLocationApp/SearchLocationApp/SearchLocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchLocationAppApp.swift
+//  SearchLocationApp
+//
+//  Created by 황규상 on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchLocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
