6d57a5f42da747b977001ea24889293cddba7211
KS <kyusang.dev@gmail.com>
2024-07-04 15:31:38 +0900

    지역화 버튼, 텍스트, 미리보기 추가

--- LocalApp/LocalApp/ContentView.swift
+++ LocalApp/LocalApp/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var changeColor = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(LocalizedStringKey("greeting-label"))  // 로컬라이즈된 문자열 키를 사용하여 텍스트를 표시
+            Text("greeting-label")
+            Button("button-label") {
+                changeColor.toggle()
+            }
         }
         .padding()
+        .background(changeColor ? Color.red : Color.yellow)
     }
 }
 
-#Preview {
+// 한국어로 기본 프리뷰 설정
+#Preview("기본 - 한국어") {
     ContentView()
+}
+
+// 영어로 프리뷰 설정
+#Preview("English") {
+    ContentView()
+        .environment(\.locale, .init(identifier: "en"))
+}
+
+// 프랑스어로 프리뷰 설정
+#Preview("French") {
+    ContentView()
+        .environment(\.locale, .init(identifier: "fr"))
+}
+
+// 스페인어로 프리뷰 설정
+#Preview("Spanish") {
+    ContentView()
+        .environment(\.locale, .init(identifier: "es"))
 }
