a0974fd3b9b91dd229b8a92701b0ba38f6c68170
KS <kyusang.dev@gmail.com>
2024-07-04 16:02:10 +0900

    Initialize PseudoLanguageApp Project

---
+++ PseudoLanguageApp/PseudoLanguageApp/PseudoLanguageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PseudoLanguageAppApp.swift
+//  PseudoLanguageApp
+//
+//  Created by 황규상 on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PseudoLanguageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
