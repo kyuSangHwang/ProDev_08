890cf9fe35685780057238cc6325c9181c6e0adb
KS <kyusang.dev@gmail.com>
2024-07-09 16:03:17 +0900

    if let 과 try? 를 이용한 간결한 에러 핸들링

--- ErrorApp/ErrorApp/ContentView.swift
+++ ErrorApp/ErrorApp/ContentView.swift
@@ -39,24 +39,30 @@ struct ContentView: View {
     }
     
     func divideFunction(numerator: Double, denominator: Double) {
-        do {
-            try checkMe(top: numerator, bottom: denominator)
-            message = "Answer = \(numerator / denominator)"
-        } catch MajorProblems.divideByZero {
-            message = "Can't divide by zero"
-        } catch MajorProblems.noNegativeNumbersPlease {
-            message = "No negative numbers, please"
-        } catch {
-            message = "Some other error occurred"
+        func divideFunction(numerator: Double, denominator: Double) {
+            // if let 과 try? 를 이용한 간결한 에러 핸들링
+            if let answer = try? checkMe(top: numerator, bottom: denominator) {
+                message = "Answer = \(numerator / denominator)"
+            } else {
+                message = "Some other error occurred"
+            }
+            // 복잡한 에러처리 대신 단일 에러 핸들링
+            //        do {
+            //            try checkMe(top: numerator, bottom: denominator)
+            //            message = "Answer = \(numerator / denominator)"
+            //        } catch {
+            //            message = "Some other error occurred"
+            //        }
         }
-    }
-    
-    func checkMe(top: Double, bottom: Double) throws {
-        guard (bottom != 0) else {
-            throw MajorProblems.divideByZero
-        }
-        guard (top > 0 && bottom > 0) else {
-            throw MajorProblems.noNegativeNumbersPlease
+        
+        func checkMe(top: Double, bottom: Double) throws -> Double {
+            guard (bottom != 0) else {
+                throw MajorProblems.divideByZero
+            }
+            guard (top > 0 && bottom > 0) else {
+                throw MajorProblems.noNegativeNumbersPlease
+            }
+            return top / bottom
         }
     }
 }
